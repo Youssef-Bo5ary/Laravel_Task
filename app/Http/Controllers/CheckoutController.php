df1d6c8822ccf18da1b2c6efa7b7f64d209c6b64
Youssef Bokhary <youssefbo5ary@gmail.com>
2025-07-31 01:05:16 +0300

    MyFirstCommit

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -6,5 +6,77 @@ use Illuminate\Http\Request;
 
 class CheckoutController extends Controller
 {
-    //
+     public function Checkout(Request $request)
+
+    {
+
+        $stripe = new \Stripe\StripeClient(env('STRIPE_SECRET'));
+
+
+
+        $redirectUrl = route('stripe.checkout.success').'?session_id={CHECKOUT_SESSION_ID}';
+
+        $response =  $stripe->checkout->sessions->create([
+
+                'success_url' => $redirectUrl,
+
+                'customer_email' => 'demo@gmail.com',
+
+                'payment_method_types' => ['link', 'card'],
+
+                'line_items' => [
+
+                    [
+
+                        'price_data'  => [
+
+                            'product_data' => [
+
+                                'name' => $request->product,
+
+                            ],
+
+                            'unit_amount'  => 100 * $request->price,
+
+                            'currency'     => 'USD',
+
+                        ],
+
+                        'quantity'    => 1
+
+                    ],
+
+                ],
+
+                'mode' => 'payment',
+
+                'allow_promotion_codes' => true
+
+            ]);
+
+
+
+        return redirect($response['url']);
+
+    }
+
+     public function stripeCheckoutSuccess(Request $request)
+
+    {
+
+        $stripe = new \Stripe\StripeClient(env('STRIPE_SECRET'));
+
+
+
+        $session = $stripe->checkout->sessions->retrieve($request->session_id);
+
+        info($session);
+
+
+
+        return redirect()->route('stripe.index')
+
+                         ->with('success', 'Payment successful.');
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\PlanController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/checkout', [CheckoutController::class, 'checkout']);
+Route::post('/checkout', [CheckoutController::class, 'afterPayment'])->name('credit-card');
 
 Route::post('/register', [AuthController::class, 'registerMethod']);
 Route::post('/login', [AuthController::class, 'login']);
