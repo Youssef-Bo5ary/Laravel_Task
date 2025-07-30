43b12cc71de4afaa7ac2c68cc705e40c3206cff0
Youssef Bokhary <youssefbo5ary@gmail.com>
2025-07-31 00:02:51 +0300

    MyFirstCommit

---
+++ app/Http/Controllers/PlanController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Plan;
+use App\Http\Requests\StorePlanRequest;
+use App\Http\Requests\UpdatePlanRequest;
+
+class PlanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Plan::paginate(10);
+    }
+
+
+}
